38d7a3d55e29b9ae6030e00d3e66f6bf6a29f513
orel-h <orel.h@quali.com>
2022-06-16 10:41:04 +0300

    Update orel_helm_script.sh

--- scripts/orel_helm_script.sh
+++ scripts/orel_helm_script.sh
@@ -1,11 +1,6 @@
-echo "creating orel dir"
-mkdir /orel
-echo "creating /orel/name.txt"
-echo "orel" > /name.txt
+#!/bin/bash
 
-echo "install nano"
-apt update
-apt install nano -y
-
-#cat /wemrwrr/w
+echo "test1"
+#mkdir /orel
+echo "test2"
 
